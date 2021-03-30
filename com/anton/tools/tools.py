1c9d873f9bcecea5b5674a458c2b2c14d55e4433
Ben <bpschreiber2003@gmail.com>
2021-03-30 14:52:42 -0700

    .

--- com/anton/tools/tools.py
+++ com/anton/tools/tools.py
@@ -4,7 +4,6 @@ import discord
 # File for helper functions that can be used throughout the program
 
 async def create_and_assign_role(t_hex: int, t_member: discord.Member):
-
     # Server the user is in
     usr_guild = t_member.guild
 
@@ -16,3 +15,35 @@ async def create_and_assign_role(t_hex: int, t_member: discord.Member):
 
     # Add that role to the user
     await t_member.add_roles(role)
+
+
+class ErrorEmbed:
+    def __init__(self,
+                 t_title="Error",
+                 t_description="You've Done it, an Error!",
+                 t_color=0xff0000
+                 ):
+        self.t_title = t_title
+        self.t_description = t_description
+        self.t_color = t_color
+
+    def embed(self) -> discord.Embed:
+        return discord.Embed(title=self.t_title,
+                             description=self.t_description,
+                             color=self.t_color)
+
+
+class SuccessEmbed:
+    def __init__(self,
+                 t_title="Successful",
+                 t_description="",
+                 t_color=0x00ff00
+                 ):
+        self.t_title = t_title
+        self.t_description = t_description
+        self.t_color = t_color
+
+    def embed(self) -> discord.Embed:
+        return discord.Embed(title=self.t_title,
+                             description=self.t_description,
+                             color=self.t_color)
